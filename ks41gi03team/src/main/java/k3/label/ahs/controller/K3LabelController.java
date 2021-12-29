19e1132ef61df8aa91be8ebe2053034145e013a5
OHHANSOO <zhdnrk124@naver.com>
2021-12-29 11:23:09 +0900

    [team03] details 1. 폴더구조 변경 2. goodsManagement 하위로 변경

--- ks41gi03team/src/main/java/k3/label/ahs/controller/K3LabelController.java
+++ ks41gi03team/src/main/java/k3/label/ahs/controller/K3LabelController.java
@@ -5,19 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value="/team03/label")
+@RequestMapping(value="/team03/goodsManagement/label")
 public class K3LabelController {
 	
 	//라벨 현황 이동
 	@GetMapping("/k3LabelList")
 	public String k3GetLabelList() {
 		
-		return "team03/label/k3LabelList";
+		return "team03/goodsManagement/label/k3LabelList";
 	}
 	//라벨 등록폼 이동
 	@GetMapping("/k3AddLabel")
 	public String k3AddLabel(){
 		
-		return "team03/label/k3AddLabel";
+		return "team03/goodsManagement/label/k3AddLabel";
 	}
 }
