543ad7a98cf72805f5d4d023a29e9498e1206cd5
lsh <lsh>
2021-12-31 16:29:29 +0900

    [team3] details : 1. 배포

--- ks41gi03team/src/main/java/k3/subject/ahs/controller/K3SubjectController.java
+++ ks41gi03team/src/main/java/k3/subject/ahs/controller/K3SubjectController.java
@@ -9,12 +9,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import k3.subject.ahs.dto.K3Subject;
 import k3.subject.ahs.service.K3SubjectService;
 
 @Controller
-@RequestMapping(value="/team03/subject")
+@RequestMapping(value="/team03/finance/subject")
 public class K3SubjectController {
 	
 	private static final Logger log = LoggerFactory.getLogger(K3SubjectController.class);
@@ -25,19 +26,50 @@ public class K3SubjectController {
 		this.k3SubjectService = k3SubjectService;
 	}
 	
-	//계정과목 등록처리(이동)
-	@PostMapping("/k3SubjectAdd")
-	public String subjectAdd(K3Subject k3Subject) {
-		int result = k3SubjectService.subjectAdd(k3Subject);
-		log.info("subjectAdd 메서드 인서트 결과 : " + result);
-		log.info("subjectAdd 메서드 인서트 결과 : " + k3Subject);
+	
+	
+	//계정과목 수정처리
+	@PostMapping("/modifySubject")
+	public String modifySubject(K3Subject k3Subject) {
+		log.info("수정화면에서 입력 받은 subject정보 : {}", k3Subject );
+		
+		//계정과목 수정
+		k3SubjectService.modifySubjectInfo(k3Subject);
+		
+		return "redirect:/team03/finance/subject/k3SubjectList";
+	}
+	
+	//계정과목 수정화면
+	@GetMapping("/k3modifySubject")
+	public String k3ModifySubject(@RequestParam(value="subjectCode", required = false) String subjectCode
+							   , Model model) {
+		//subjectCode 콘솔에 출력(log4j)
+		log.info("modifySubject subjectCode: {}", subjectCode);
+		
+		//계정과목의 정보
+		if(subjectCode != null && !"".equals(subjectCode)) {
+			K3Subject subjectInfo = k3SubjectService.getSubjectInfoBySubjectCode(subjectCode);
+			model.addAttribute("subjectInfo", subjectInfo);
+		}
+		model.addAttribute("title", "계정과목수정화면");
+		return "team03/finance/subject/k3ModifySubject";
+	}
+	
+	//계정과목 등록처리
+	@PostMapping("/k3addSubject")
+	public String addSubject(K3Subject k3Subject) {
+		int result = k3SubjectService.addSubject(k3Subject);
+		log.info("addSubject 메서드 인서트 결과 : " + result);
+		log.info("addSubject 메서드 인서트 결과 : " + k3Subject);
 		return "redirect:/team03/finance/subject/k3SubjectList";
 	}
 	
 	//계정과목 등록화면(이동)
-	@GetMapping("/k3SubjectAdd")
-	public String goToSubjectAddForm(Model model) {
-		return "/team03/finance/subject/k3SubjectAdd";
+	@GetMapping("/k3AddSubject")
+	public String goToAddSubjectForm(Model model) {
+		model.addAttribute("title", "계정과목 관리");
+		model.addAttribute("subtitle", "계정과목 등록");
+		return "/team03/finance/subject/k3AddSubject";
 	}
 	
 	//계정과목 현황(초기화면)
