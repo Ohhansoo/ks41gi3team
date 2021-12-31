465ba22e737843aab8bb3910f90a86e4f20884bb
leeeunbin <leb4610@naver.com>
2021-12-31 11:38:02 +0900

    [team03] leb modify Contract/Contractor sidebar

--- ks41gi03team/src/main/java/k3/contractor/ahs/controller/K3ContractorController.java
+++ ks41gi03team/src/main/java/k3/contractor/ahs/controller/K3ContractorController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import k3.contractor.ahs.dto.K3Contractor;
+import k3.contractor.ahs.dto.K3ContractorDetail;
 import k3.contractor.ahs.service.K3ContractorService;
 
 @Controller
@@ -20,18 +22,34 @@ public class K3ContractorController {
 		this.k3ContractorService = k3ContractorService;
 	}
 	
-	//거래처 등록
-	@GetMapping("/addContractor")
+	//거래처 등록으로 화면전환
+	@GetMapping("/k3AddContractor")
 	public String K3GetAddContractor(Model model) {
-		
 		model.addAttribute("title", "거래처 관리");
 		model.addAttribute("subtitle", "거래처 등록");
 		
-		return "/team03/contractorContract/Contractor/addContractor";
+		return "/team03/contractorContract/Contractor/k3AddContractor";
+	}
+	
+	//거래처 등록
+	@PostMapping("/k3AddContractor")
+	public String k3AddContractor(K3ContractorDetail k3ContractorDetail) {
+		
+		System.out.println("ContractorController 거래처 등록 화면에서 입력받은 값" + k3ContractorDetail);
+		
+		String contractorCode = k3ContractorDetail.getContractorCode();
+		String contractorBusinessNum = k3ContractorDetail.getContractorBusinessNum();
+		
+		if(contractorCode != null && contractorBusinessNum != null 
+			&& !"".equals(contractorCode) && !"".equals(contractorBusinessNum)) {
+			k3ContractorService.k3AddContractor(k3ContractorDetail);
+		}
+		
+		return "redirect:/team03/contractorContract/Contractor/k3SearchContractor";
 	}
 	
 	//거래처 현황에 거래처 가져오기
-	@GetMapping("/searchContractor")
+	@GetMapping("/k3SearchContractor")
 	public String K3GetContractorSearch(Model model) {
 		List<K3Contractor> contractorList = k3ContractorService.K3GetContractorList();
 
@@ -39,14 +57,14 @@ public class K3ContractorController {
 		model.addAttribute("subtitle", "거래처 현황");
 		model.addAttribute("contractorList", contractorList);
 
-		return "/team03/contractorContract/Contractor/searchContractor";
+		return "/team03/contractorContract/Contractor/k3SearchContractor";
 	}
 	
 	//거래처 수정
-	@GetMapping("/modifyContractor")
+	@GetMapping("/k3ModifyContractor")
 	public String k3GetModifyContractor(Model model) {
 		model.addAttribute("title","거래처 관리");
 		model.addAttribute("subtitle", "거래처 수정");
-		return "/team03/contractorContract/Contractor/modifyContractor";
+		return "/team03/contractorContract/Contractor/k3ModifyContractor";
 	}
 }
