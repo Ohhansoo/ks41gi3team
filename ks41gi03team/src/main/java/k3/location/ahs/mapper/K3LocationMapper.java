704b3f1a84302c41b17fdc2649a85b0ed3edd1d5
qpfn1 <qpfn1@192.168.219.100>
2022-01-04 23:57:54 +0900

    [team03] detail: 1.warehouse패키지 파일 수정 2.location 수정

--- ks41gi03team/src/main/java/k3/location/ahs/mapper/K3LocationMapper.java
+++ ks41gi03team/src/main/java/k3/location/ahs/mapper/K3LocationMapper.java
@@ -2,7 +2,12 @@ package k3.location.ahs.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import k3.location.ahs.dto.K3Location;
+
 @Mapper
 public interface K3LocationMapper {
 
+	/* public int k3AddLocationCode(String allCode); */
+	
+	public int k3AddLocation(String allCode);
 }
--- ks41gi03team/src/main/java/k3/location/ahs/service/K3LocationServise.java
+++ ks41gi03team/src/main/java/k3/location/ahs/service/K3LocationServise.java
@@ -2,7 +2,22 @@ package k3.location.ahs.service;
 
 import org.springframework.stereotype.Service;
 
+import k3.location.ahs.dto.K3Location;
+import k3.location.ahs.mapper.K3LocationMapper;
+
 @Service
 public class K3LocationServise {
-		
+	
+	private K3LocationMapper k3LocationMapper;
+	public K3LocationServise(K3LocationMapper k3LocationMapper) {
+		this.k3LocationMapper = k3LocationMapper;
+	}
+
+	/*
+	 * public int k3AddLocationCode(String allCode) { return
+	 * k3LocationMapper.k3AddLocationCode(allCode); }
+	 */
+	public int k3AddLocation(String allCode) {
+		return k3LocationMapper.k3AddLocation(allCode);
+	}
 }
