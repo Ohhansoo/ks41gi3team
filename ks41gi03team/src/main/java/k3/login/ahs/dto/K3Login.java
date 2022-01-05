701a2bab80b486aeac06e8e55184baff0a545a3b
sah <ECS@DESKTOP-048HU3O>
2022-01-05 17:17:51 +0900

    [team03] song ahyun details 1. login수정

--- ks41gi03team/src/main/java/k3/login/ahs/dto/K3Login.java
+++ ks41gi03team/src/main/java/k3/login/ahs/dto/K3Login.java
@@ -2,5 +2,6 @@ package k3.login.ahs.dto;
 
 public class K3Login {
 	
+	
 
 }
--- ks41gi03team/src/main/java/k3/memberuser/ahs/mapper/K3MemberUserMapper.java
+++ ks41gi03team/src/main/java/k3/memberuser/ahs/mapper/K3MemberUserMapper.java
@@ -9,17 +9,23 @@ import k3.memberuser.ahs.dto.K3MemberUser;
 @Mapper
 public interface K3MemberUserMapper {
 	
-	//직원 정보 찾기(수정)
-	public K3MemberUser K3MemberUserInfoBycode(String memberId);
 	
 	//직원중복체크
-	public int getMemberInfoByMemberId(String memberId);
+	public int getMemberUserByMemberId(String memberId);
 	
 	//직원등록
 	public int AddMemberUser(K3MemberUser k3MemberUser);
-
-	//회원전체 조회
-	public List<K3MemberUser> getMemberUserList();
 	
+	//직원 정보 수정
+	public int modifyMemberUserInfo(K3MemberUser k3MemberUser);
+
+	//회원조회
+	public int getMemberInfoByMemberId(String memberId);
+	
+	//직원 조회
+	public List<K3MemberUser> getMemberUserListBySearchKey(String searchKey, String searchValue);
+	
+	//회원전체 조회(초기)
+	public List<K3MemberUser> getMemberUserList();
 }
 //K3MemberUser
