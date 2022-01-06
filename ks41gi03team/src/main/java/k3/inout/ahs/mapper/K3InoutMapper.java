e85664c67c9c7436cfe5d22f0903b4487f0cf699
lsh <lsh>
2022-01-06 16:21:12 +0900

    [team3]
    details :
    1. inout 수정

--- ks41gi03team/src/main/java/k3/inout/ahs/mapper/K3InoutMapper.java
+++ ks41gi03team/src/main/java/k3/inout/ahs/mapper/K3InoutMapper.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import k3.inout.ahs.dto.K3Inout;
+import k3.subject.ahs.dto.K3Subject;
 
 
 @Mapper
 public interface K3InoutMapper {
+	
+
+	//계정과목 수정 폼으로 이동
+	public K3Inout getModifyInout(String inoutRec);	
 	
 	//입출금 내역 등록처리
 	public int addInout(K3Inout k3Inout);
