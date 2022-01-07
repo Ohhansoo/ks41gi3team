d9916b24917722201580bf2faa96ede190d473ee
OHHANSOO <zhdnrk124@naver.com>
2022-01-07 16:42:56 +0900

    [team03] details: 1. 입고요청 등록 처리 완료. 2. 모달창 작업완료.

--- ks41gi03team/src/main/java/k3/warehousing/ahs/mapper/K3WarehousingMapper.java
+++ ks41gi03team/src/main/java/k3/warehousing/ahs/mapper/K3WarehousingMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import k3.contract.ahs.dto.K3Contract;
 import k3.warehousing.ahs.dto.K3Warehousing;
 import k3.warehousing.ahs.dto.K3WarehousingSort;
 
@@ -11,6 +12,8 @@ import k3.warehousing.ahs.dto.K3WarehousingSort;
 public interface K3WarehousingMapper {
 	//입고 요청 등록 처리
 	public int k3AddWarehousing(K3Warehousing k3Warehousing);
+	//계약처명 불러오기
+	public List<K3Contract> k3FindContractorNameByCode();
 	//입고 요청 현황
 	public List<K3Warehousing> k3RequestAllowWarehousing();
 	//입고 분류 현황(요청목록)
