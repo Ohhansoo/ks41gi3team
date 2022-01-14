dace1577822827c56683bcc62ac864d4e2216989
ECS <ECS@DESKTOP-048HU3O>
2022-01-14 17:23:32 +0900

    [team03] detail: 1.dock파일삭제 2.반품하자 패키지 파일 삭제 3.damageError 패키지 파일 4.damageErrorList s완료

---
+++ ks41gi03team/src/main/java/k3/damegeError/ahs/controller/K3DamegeErrorController.java
@@ -0,0 +1,39 @@
+package k3.damegeError.ahs.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import k3.damegeError.ahs.dto.K3DamageError;
+import k3.damegeError.ahs.service.K3DamageErrorService;
+
+@Controller
+@RequestMapping(value="/team03/goodsManagement/damageError")
+public class K3DamegeErrorController {
+	/**
+	 * 의존성 주입 생성자 메소드
+	 */
+	private K3DamageErrorService k3DamageErrorService;
+	public K3DamegeErrorController(K3DamageErrorService k3DamageErrorService) {
+		this.k3DamageErrorService = k3DamageErrorService;
+	}
+	
+	/**
+	 * AS대기현황
+	 */
+	@GetMapping("/k3DamageErrorList")
+	public String k3DamageErrorList(K3DamageError k3DamageErrorIn,K3DamageError k3DamageErrorOut,  Model model) {
+		List<K3DamageError> k3DamageErrorListIn = k3DamageErrorService.k3DamageErrorListIn(k3DamageErrorIn);
+		List<K3DamageError> k3DamageErrorListOut = k3DamageErrorService.k3DamageErrorListOut(k3DamageErrorOut);
+		
+		System.out.println("controller---------->>>"+k3DamageErrorListIn);
+		model.addAttribute("title","AS");
+		model.addAttribute("k3DamageErrorListIn",k3DamageErrorListIn);
+		model.addAttribute("k3DamageErrorListOut",k3DamageErrorListOut);
+		return "/team03/goodsManagement/damageError/k3DamageErrorList";
+	}
+}
